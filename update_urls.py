381084ee6ee4dd8fcc5249fb35d513e65a246f48
Torsten Sommer <torsten.sommer@3ds.com>
2025-12-03 14:27:47 +0100

    Write index.html to build directory

--- update_urls.py
+++ update_urls.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 lines = []
 index_path = Path(__file__).parent.parent / "index.html"
+output_path = Path(__file__).parent.parent.parent / "build" / "index.html"
 
 urls = {
     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css":
@@ -20,5 +21,5 @@ with open(index_path, mode="r", encoding="utf-8") as index:
             line = line.replace(k, v)
         lines.append(line)
 
-with open(index_path, "w", encoding="utf-8") as index:
+with open(output_path, "w", encoding="utf-8") as index:
     index.writelines(lines)
